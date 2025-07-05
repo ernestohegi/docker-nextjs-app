ccfb1d9ea8737071fad309e9d708e0ac8f22d801
Ernesto <ernesto.hegi@gmail.com>
2025-07-05 15:32:46 +0100

    Removes hostname

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,5 @@ export default function Page() {
   return <main>
     <h1>Hello, Next.js!</h1>
     <p>This is a simple page using Next.js.</p>
-    <p>You are connected to {process.env.HOSTNAME || 'no host'}.</p>
   </main>;
 }
